1b9131c463ad6767b40bb8ef3c9cf7eb64d5bb77
faizankhalid <fk5095129@gmail.com>
2026-06-27 16:04:55 +0500

    push

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b1bafff1'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1lkf9q6kjmc"
+    "revision": "0.qfub9ropp1g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
